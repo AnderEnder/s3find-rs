d5022b1bbd84c1cad44e6e049101b13d6bc9de99
Andrii Radyk <ander.ender@gmail.com>
2019-05-30 00:12:23 +0200

    upgrade version_check to 0.9 (#83)
    * upgrade version_check to 0.9
    * tarpaulin changes

--- build.rs
+++ build.rs
@@ -6,14 +6,9 @@ use structopt::clap::Shell;
 include!("src/arg.rs");
 
 fn main() {
-    match version_check::is_min_version("1.31") {
-        // rustc >= 1.30
-        Some((true, _)) => {}
-        // rustc < 1.20 or can't figure it out
-        _ => {
-            writeln!(&mut io::stderr(), "This crate requires rustc >= 1.31").unwrap();
-            exit(1);
-        }
+    if !version_check::is_min_version("1.31").unwrap_or(false) {
+        writeln!(&mut io::stderr(), "This crate requires rustc >= 1.31").unwrap();
+        exit(1);
     }
 
     let var = std::env::var_os("SHELL_COMPLETIONS_DIR").or(std::env::var_os("OUT_DIR"));
